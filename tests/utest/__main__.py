ca6a6dff207bac3db0a28c6ac6c7034ea590df6b
Tatu Kairi <tatu.kairi@gmail.com>
2015-07-08 16:57:49 +0300

    added initial few unit tests

---
+++ tests/utest/__main__.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+from os.path import abspath, dirname, join as path_join
+
+directory = dirname(__file__)
+path = path_join(abspath(path_join(directory, '..', '..', 'src')))
+sys.path.insert(1, path)
+
+try:
+    import mock
+except ImportError:
+    raise ImportError('Please install mock')
+
+unittest.TextTestRunner().run(unittest.TestLoader().discover(directory))
+
---
+++ tests/utest/test_keyboard.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from mock import patch, MagicMock
+
+import pyautogui
+
+class TestKeyboard(TestCase):
+    def setUp(self):
+        self.mock = MagicMock()
+        for method in ['typewrite', 'press', 'keyDown', 'keyUp']:
+            setattr(self.mock, method, MagicMock())
+        self.mock.KEYBOARD_KEYS = pyautogui.KEYBOARD_KEYS
+        self.patcher = patch.dict('sys.modules', {'pyautogui' : self.mock})
+        self.patcher.start()
+        from ImageHorizonLibrary import ImageHorizonLibrary
+        self.lib = ImageHorizonLibrary()
+
+    def tearDown(self):
+        self.mock.clear()
+        self.patcher.stop()
+
+    def test_type_with_text(self):
+        self.lib.type('hey you fool')
+        self.mock.typewrite.assert_called_once_with('hey you fool')
+
+    def test_type_with_text_and_keys(self):
+        self.lib.type('I love you', 'Key.ENTER')
+        self.mock.typewrite.assert_called_once_with('I love you')
+        self.mock.press.assert_called_once_with('enter')
+
+    def test_type_with_keys_down(self):
+        self.lib.type_with_keys_down('hello', 'key.shift')
+        self.mock.keyDown.assert_called_once_with('shift', pause=0.0)
+        self.mock.typewrite.assert_called_once_with('hello', pause=0.0,
+                                                    interval=0.0)
+        self.mock.keyUp.assert_called_once_with('shift', pause=0.0)
+
+    def test_press_combination(self):
+        from ImageHorizonLibrary import ImageHorizonLibrary
+        with patch('ImageHorizonLibrary.ImageHorizonLibrary._press',
+                   autospec=True) as press_mock:
+            lib = ImageHorizonLibrary()
+            lib.press_combination('Key.ctrl', 'A')
+            press_mock.assert_called_once_with(lib, 'Key.ctrl', 'A')
+
