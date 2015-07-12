08d6c6b4024cdbc022a84e2eadf2f267aebe6238
Tatu Kairi <tatu.kairi@gmail.com>
2015-07-12 17:41:25 +0300

    removed old pykuli dir

--- pykuli/__init__.py
+++
@@ -1,2 +0,0 @@
-from keywords import *
-from tools import *
--- pykuli/tests/windows/constants.py
+++
@@ -1,3 +0,0 @@
-import os.path
-
-REFERENCE_IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'reference_images')
