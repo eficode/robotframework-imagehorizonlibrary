e4a03c61109e8005f26fb5fcca31fd105fc61795
Markus Suonto <markus.suonto@eficode.com>
2016-01-20 14:39:08 +0200

    Remove unneccessary bool conversion

--- tests/utest/run_tests.py
+++ tests/utest/run_tests.py
@@ -19,4 +19,4 @@ if len(sys.argv) > 1 and 'verbosity=' in sys.argv[1]:
 else:
     verbosity = 1
 
-sys.exit(not bool(TextTestRunner(verbosity=verbosity).run(TestLoader().discover(directory)).wasSuccessful()))
+sys.exit(not TextTestRunner(verbosity=verbosity).run(TestLoader().discover(directory)).wasSuccessful())
