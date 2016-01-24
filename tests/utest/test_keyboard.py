4e44a4cf2cabd9c3be36346661df25bb90f26314
Tatu Kairi <tatu.kairi@gmail.com>
2016-01-24 10:25:25 +0200

    fix test that never failed

--- tests/utest/test_keyboard.py
+++ tests/utest/test_keyboard.py
@@ -3,37 +3,30 @@ from unittest import TestCase
 from mock import patch, MagicMock
 
 
-# For travis: pyautogui import fails without a display
-try:
-    from pyautogui import KEYBOARD_KEYS
-except Exception, e:
-    # Cannot import DisplayNameError from pyautogui
-    if type(e).__name__ != 'DisplayNameError':
-        raise e
-    KEYBOARD_KEYS = [
-        '\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
-        ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-        'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-        'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
-        'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
-        'browserback', 'browserfavorites', 'browserforward', 'browserhome',
-        'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
-        'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
-        'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
-        'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
-        'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
-        'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
-        'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
-        'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
-        'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
-        'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
-        'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
-        'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
-        'shift', 'shiftleft', 'shiftright', 'sleep', 'stop', 'subtract', 'tab',
-        'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 
-        'yen', 'command', 'option', 'optionleft', 'optionright'
-    ]
+KEYBOARD_KEYS = [
+    '\\t', '\\n', '\\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+    ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+    'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+    'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+    'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+    'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+    'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+    'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+    'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+    'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+    'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert',
+    'junja', 'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+    'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+    'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+    'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+    'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+    'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+    'shift', 'shiftleft', 'shiftright', 'sleep', 'stop', 'subtract', 'tab',
+    'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright',
+    'yen', 'command', 'option', 'optionleft'
+]
 
 class TestKeyboard(TestCase):
     def setUp(self):
@@ -81,15 +74,11 @@ class TestKeyboard(TestCase):
     def test_type_with_keys_down_with_invalid_keys(self):
         from ImageHorizonLibrary import KeyboardException
 
-        expected_msg = ', '.join(self.mock.KEYBOARD_KEYS)
-
-        with self.assertRaises(KeyboardException) as e:
+        expected_msg = ('Invalid keyboard key "enter", valid keyboard keys '
+                        'are:\n%r' % ', '.join(self.mock.KEYBOARD_KEYS))
+        with self.assertRaises(KeyboardException) as cm:
             self.lib.type_with_keys_down('sometext', 'enter')
-            self.assertTrue(e.message.endswith(expected_msg))
-
-        with self.assertRaises(KeyboardException) as e:
-            self.lib.type_with_keys_down('sometext', 'enter')
-            self.assertTrue(e.message.endswith(expected_msg))
+        self.assertEquals(cm.exception.message, expected_msg)
 
     def test_press_combination(self):
             self.lib.press_combination('Key.ctrl', 'A')
