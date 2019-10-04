c23988034b9a426c630b9ed068b139ce3ac2ec37
Jon Koser <jon@polarity.io>
2019-10-04 16:32:34 -0500

    Unit test for confidence

--- tests/utest/test_recognize_images.py
+++ tests/utest/test_recognize_images.py
@@ -22,6 +22,15 @@ class TestRecognizeImages(TestCase):
         self.mock.reset_mock()
         self.patcher.stop()
 
+    def test_find_with_confidence(self):
+        self.lib.reference_folder = path_join(CURDIR, 'symbolic_link')
+        self.lib.set_confidence(0.5)
+        self.lib.has_cv = True
+        self.lib.locate('mY_PiCtURe')
+        expected_path = path_join(CURDIR, 'symbolic_link', 'my_picture.png')
+        self.mock.locateOnScreen.assert_called_once_with(expected_path, confidence=0.5)
+        self.mock.reset_mock()
+
     def test_click_image(self):
         with patch(self.locate, return_value=(0, 0)):
             self.lib.click_image('my_picture')
