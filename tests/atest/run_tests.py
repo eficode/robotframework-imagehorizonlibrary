25e8ad027d91f8f1a1fde40e07ad9d05b9578d31
Matej Cotman <matej.cotman@eficode.com>
2020-04-16 13:11:49 +0300

    feat(tests): rewrite

--- tests/atest/run_tests.py
+++ tests/atest/run_tests.py
@@ -2,21 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-from os.path import abspath, dirname, join as path_join
+import os
 
 from robot import run_cli
 
-if __name__ == '__main__':
-    curdir = abspath(dirname(__file__))
-    sys.path.insert(1, abspath(path_join(curdir, '..', '..', 'src')))
-    from ImageHorizonLibrary.utils import *
-
-    if is_windows():
-        tag = 'windows'
-    elif is_mac():
-        tag = 'mac'
-    elif is_linux():
-        tag = 'linux'
-
-run_cli(sys.argv[1:] + ['--include', tag, '.'])
+run_cli(sys.argv[1:] + [os.path.dirname(__file__)])
